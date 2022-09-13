886e00e99e60e4aef53c75bea2120ac07deaaa95
Phillip Burgess <paintyourdragon@dslextreme.com>
2022-09-13 13:21:08 -0700

    Some cleanup

--- pinguin.py
+++ pinguin.py
@@ -2,7 +2,11 @@
 Adafruit Pinguin -- an EAGLE silkscreen label generator for nicer fonts
 with minimal intervention. Reads an EAGLE .brd file, looking for text
 objects on specific layers, adds raster equivalents (in nicer fonts) on
-different layers, which can be merged with the normal silk output.
+different layers; include the latter with the normal silk output when
+producing CAM files.
+
+Forgive the mess, this is hastily-written muck and not Pythonic or
+whatever; like only three people will be using it anyway.
 """
 
 import argparse
@@ -10,25 +14,28 @@ import os
 import xml.etree.ElementTree as ET
 from PIL import Image, ImageFont, ImageDraw
 
-# Some global configurables ----
+# GLOBAL CONFIGURABLES -----------------------------------------------------
 
-# Configure .brd layers used for input and output
-TOP_OUT = 170  #    Top silk output (will be added if not present)
-BOTTOM_OUT = 171  # Bottom silk output (will be added if not present)
-TOP_IN = 172  #     Top labels input
-BOTTOM_IN = 173  #  Bottom labels input
+TOP_OUT = 170  #    Top silk output layer (will be added if not present)
+BOTTOM_OUT = 171  # Bottom silk output layer "
+TOP_IN = 172  #     Top labels input layer "
+BOTTOM_IN = 173  #  Bottom labels input layer "
 # Additional "IN" layers might get added here later as a cheap
 # way of specifying effects like inverted text in a box.
 
+label_num = 0  # Counter for labels, incremented as they're added to file
+
+# CONFIGURE INPUT ----------------------------------------------------------
+
 parser = argparse.ArgumentParser()
-parser.add_argument('filename', nargs='?', default="AHT20.brd")
-parser.add_argument('-vfont', type=str, default="fonts/GNU/FreeSans.ttf")
-parser.add_argument('-pfont', type=str, default="fonts/Arimo/static/Arimo-Regular.ttf")
-parser.add_argument('-ffont', type=str, default="fonts/GNU/FreeMono.ttf")
-parser.add_argument('-vscale', type=float, default=1.414)
-parser.add_argument('-pscale', type=float, default=1.414)
-parser.add_argument('-fscale', type=float, default=1.414)
-parser.add_argument('-dpi', type=int, default=1200)
+parser.add_argument("filename", nargs="?", default="AHT20.brd")
+parser.add_argument("-vfont", type=str, default="fonts/GNU/FreeSans.ttf")
+parser.add_argument("-pfont", type=str, default="fonts/Arimo/static/Arimo-Regular.ttf")
+parser.add_argument("-ffont", type=str, default="fonts/GNU/FreeMono.ttf")
+parser.add_argument("-vscale", type=float, default=4 / 3)
+parser.add_argument("-pscale", type=float, default=2 ** 0.5)
+parser.add_argument("-fscale", type=float, default=2 ** 0.5)
+parser.add_argument("-dpi", type=int, default=1200)
 args = parser.parse_args()
 infile = args.filename
 path = os.path.split(args.filename)
@@ -37,17 +44,18 @@ if idx < 0:
     print("Input must be a .brd file")
     exit(0)
 if len(path[0]):
-    outfile = path[0] + '/' + path[1][:idx] + "_out.brd"
+    outfile = path[0] + "/" + path[1][:idx] + "_out.brd"
 else:
     outfile = path[1][:idx] + "_out.brd"
-font_spec = ((args.vfont, args.vscale),
-             (args.pfont, args.pscale),
-             (args.ffont, args.fscale))
-mm_to_px = args.dpi / 25.4
-px_to_mm = 25.4 / args.dpi
+font_spec = (
+    (args.vfont, args.vscale),  # Font files and scale factors for
+    (args.pfont, args.pscale),  # vector, proportional & fixed fonts.
+    (args.ffont, args.fscale),
+)  # DO NOT reorder list.
+mm_to_px = args.dpi / 25.4  # For scaling text to pixel units
+px_to_mm = 25.4 / args.dpi  # For scaling pixels back to document
 
-label_num = 0  # Library parts are added incrementally
-
+# UTILITY FUNCS ------------------------------------------------------------
 
 def layer_find_add(parent, list, number, name, color):
     """Search EAGLE tree for layer by number.
@@ -102,7 +110,6 @@ def rectify(parent, image, anchor_x, anchor_y):
         if pixel_state > 0:
             rect(parent, start_x, image.width, row, anchor_x, anchor_y)
 
-
 # Order of this list is important, don't mess with
 align_list = [
     "bottom-left",
@@ -128,18 +135,9 @@ def process_layer(in_texts, in_layer, out_elements, out_packages, out_layer):
             # Rasterize it and place in the library, add an
             # element to the .brd output layer referencing it.
             text_font = font_list.index(text.get("font", "proportional"))
-# TO DO: units here are in pixels...need to handle
-# DPI conversion here, NOT in the rect function!
-# (though that might need in/mm conversion)
-# ImageFont size is in pixels...useless without DPI.
-# I'm going to GUESS it's 72 or 96.
-# Oh, OK - text.get(size) is likely document units
-# (might be in or mm).
-# DPI is inches.
-# scale former by latter to get pixel units,
-# then always replicate at 1:1
-            #text_size = int(float(text.get("size")) * font_spec[text_font][1] + 0.5)
-            text_size = int(float(text.get("size")) * font_spec[text_font][1] * mm_to_px + 0.5)
+            text_size = int(
+                float(text.get("size")) * font_spec[text_font][1] * mm_to_px + 0.5
+            )
             font = ImageFont.truetype(font_spec[text_font][0], text_size)
             metrics = font.getmetrics()
             box = font.getbbox(
@@ -156,9 +154,7 @@ def process_layer(in_texts, in_layer, out_elements, out_packages, out_layer):
             height = box[3] - box[1] + 1
             image = Image.new("1", (width, height), color=0)
             draw = ImageDraw.Draw(image)
-            draw.text(
-                (-box[0], -box[1]), text.text, font=font, fill=1, features=None
-            )
+            draw.text((-box[0], -box[1]), text.text, font=font, fill=1, features=None)
             text_align = align_list.index(text.get("align", "bottom-left"))
             anchor_horiz = text_align % 3
             anchor_vert = text_align // 3
@@ -194,8 +190,7 @@ def process_layer(in_texts, in_layer, out_elements, out_packages, out_layer):
             label_num += 1
 
 
-# -----
-
+# DO THE THING -------------------------------------------------------------
 
 brd_tree = ET.parse(infile)
 brd_root = brd_tree.getroot()
@@ -237,24 +232,10 @@ bottom_in = layer_find_add(brd_layers, layer_list, BOTTOM_IN, "Pinguin_bIn", 1)
 # Sort .brd layers list so Pinguin-added items aren't at end in EAGLE menu
 brd_layers[:] = sorted(brd_layers, key=lambda child: int(child.get("number")))
 
-brd_grid = brd_root.findall("drawing/grid")[0]
-print(brd_grid.get("unit"))
-# could be "mic", "mm", "mil" or "inch"
-# micrometer, millimeter, mills (1/1000 in), inch
-# If inch, text scale is equal to DPI setting
-# if mills, DPI / 1000
-# etc.
-print(brd_grid.get("distance"))
-# Text units are always mm regardless of doc units setting.
-# (Properties dialog scales so units = doc units, but file is mm)
-
-
 # Get list of text objects in the .brd file
-
 brd_elements = brd_root.findall("drawing/board/elements")[0]  # <elements> in .brd
 brd_plain = brd_root.findall("drawing/board/plain")[0]
 brd_texts = brd_root.findall("drawing/board/plain/text")
-# brd_texts = brd_plain.findall("text")
 # Need to do some check-if-exist stuff here
 brd_libraries = brd_root.findall("drawing/board/libraries")[0]  # <libraries> in .brd
 brd_library_list = brd_libraries.findall("library")  # List of <library> items
@@ -266,6 +247,8 @@ brd_packages = ET.SubElement(brd_library, "packages")
 
 process_layer(brd_texts, TOP_IN, brd_elements, brd_packages, TOP_OUT)
 process_layer(brd_texts, BOTTOM_IN, brd_elements, brd_packages, BOTTOM_OUT)
+
+# WRITE RESULTS ------------------------------------------------------------
 
 # ElementTree by default doesn't indent XML. There's an option in Python >= 3.9
 # to do this, but it's not present in older versions (e.g. macOS at the moment).
@@ -280,13 +263,13 @@ except:
 brd_tree.write(outfile, encoding="utf-8", xml_declaration=True)
 
 
-# ------------------------------
+# NOTES --------------------------------------------------------------------
 
-
+# Stuff from eagle.dtd
 # Default alignment (None) is lower left
 # center alignment is centered on BOTH axes
-# <!ENTITY % TextFont          "(vector | proportional | fixed)">
-# <!ENTITY % Align             "(bottom-left | bottom-center | bottom-right | center-left | center | center-right | top-left | top-center | top-right)">
+# <!ENTITY % TextFont "(vector | proportional | fixed)">
+# <!ENTITY % Align    "(bottom-left | bottom-center | bottom-right | center-left | center | center-right | top-left | top-center | top-right)">
 # (is position of anchor relative to text)
 # <!ATTLIST text
 #           x             %Coord;        #REQUIRED
@@ -295,25 +278,12 @@ brd_tree.write(outfile, encoding="utf-8", xml_declaration=True)
 #           layer         %Layer;        #REQUIRED
 #           font          %TextFont;     "proportional"
 #           ratio         %Int;          "8" <- stroke thickness
-#           rot           %Rotation;     "R0" <- degrees (not just 90 inc)
+#           rot           %Rotation;     "R0" <- degrees
 #           align         %Align;        "bottom-left"
 #           distance      %Int;          "50" <- line distance
 #           grouprefs     IDREFS         #IMPLIED
 #           >
 # Ratio is ignored on proportional font
-
-#    # First 4 are required, rest are optional and have defaults if not present
-#    print(
-#        t.get("x"),
-#        t.get("y"),
-#        t.get("size"),
-#        t.get("layer"),
-#        t.get("font", "proportional"),
-#        t.get("ratio", "8"),
-#        t.get("rot", "R0"),
-#        t.get("align", "bottom-left"),
-#        t.text,
-#    )
 
 # <!ATTLIST layer
 #          number        %Layer;        #REQUIRED
